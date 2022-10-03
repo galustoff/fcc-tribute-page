e612b4147ddfba48360bdd882f8cb7b21b6c0c4b
galustoff <galustoff.dan@yandex.ru>
2022-10-03 12:34:18 +0300

    production version

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      }
     }),
     new MiniCssExtractPlugin(),
     new CopyWebpackPlugin({
